f303371bc80f2ff87b0c28a0a8fd6a75d03fa97b
zed <zed@kolk.me>
2020-12-16 23:45:05 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {}.
+      :hero => {},
       :hero_friends => []
       },
     :capulet => {
