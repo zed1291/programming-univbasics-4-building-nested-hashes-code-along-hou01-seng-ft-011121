11fdbcf486f04f69e11b41ae1dec9d854412ad27
zed <zed@kolk.me>
2020-12-16 23:53:12 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -5,12 +5,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        name: "Benvolio",
+        {name: "Benvolio",
         age: "17",
-        attitude:"worried"
-        name: "Mercutio",
+        attitude:"worried"}
+        {name: "Mercutio",
         age: "18",
-        attitude:"hot-headed"
+        attitude:"hot-headed"}
       ]
    },
    :capulet => {
@@ -18,7 +18,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-
+        {name: "Steven",
+        age: "30",
+        attitude:"confused"}
+        {name: "Nurse",
+        age: "44",
+        attitude:"worried"}
       ]
    }
 }
