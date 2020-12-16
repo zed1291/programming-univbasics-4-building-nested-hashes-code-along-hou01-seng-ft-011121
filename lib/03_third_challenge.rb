9030736c2e84e19ba5310e0b31ca8e254f7e660e
zed <zed@kolk.me>
2020-12-16 23:49:08 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         },
       :hero => {
         name: "Romeo",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
@@ -27,7 +27,7 @@ def third_challenge
         },
       :heroine => {
         name: "Juliet",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :heroine_friends => []
