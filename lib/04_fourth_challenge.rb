72e1e9238c5b9852578c0fbb79d63df75174b96a
zed <zed@kolk.me>
2020-12-16 23:53:23 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -7,7 +7,7 @@ def fourth_challenge
       :hero_friends => [
         {name: "Benvolio",
         age: "17",
-        attitude:"worried"}
+        attitude:"worried"},
         {name: "Mercutio",
         age: "18",
         attitude:"hot-headed"}
@@ -20,7 +20,7 @@ def fourth_challenge
       :heroine_friends => [
         {name: "Steven",
         age: "30",
-        attitude:"confused"}
+        attitude:"confused"},
         {name: "Nurse",
         age: "44",
         attitude:"worried"}
