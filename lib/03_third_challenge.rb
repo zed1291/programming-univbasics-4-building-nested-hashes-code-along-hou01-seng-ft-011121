b69ab7f36ed1d3bf99075ab2f32f03f46110ab4e
zed <zed@kolk.me>
2020-12-16 23:45:25 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,25 +2,25 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+
         },
       :matriarch => {
-        #your key/value pairs here
+
         },
       :hero => {
-        #your key/value pairs here
+
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+
         },
       :matriarch => {
-        #your key/value pairs here
+
         },
       :heroine => {
-        #your key/value pairs here
+
         },
       :heroine_friends => []
    }
